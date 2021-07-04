7e25db054919a7b2d7cea075d3c60e66bdb6e9f3
anumrafaqat66 <anumrafaqat97@gmail.com>
2021-07-03 22:28:54 -0700

    updated

--- application/views/do/personal_data.php
+++ application/views/do/personal_data.php
@@ -345,11 +345,11 @@
 
                                     <div class="form-group row justify-content-center">
                                         <div class="col-sm-4">
-                                            <button type="submit" class="btn btn-primary btn-user btn-block" id="add_btn">
+                                            <button type="button" class="btn btn-primary btn-user btn-block" id="add_btn">
                                                 <!-- <i class="fab fa-google fa-fw"></i>  -->
                                                 Submit
                                             </button>
-                                            <span id="show_error_new" style="font-size:10px; color:red; display:none">&nbsp;&nbsp;Please check errors*</span>
+                                            <span id="show_error_new" style="font-size:10px; color:red; display:none">&nbsp;&nbsp;Fill in the required fields*</span>
                                         </div>
                                     </div>
 
@@ -407,45 +407,38 @@
         $('#add_btn').attr('disabled', true);
         var validate = 0;
 
-        var oc_no = $('#oc_no').val();
+        var officer_name = $('#officer_name').val();
         var pno = $('#pno').val();
-        var name = $('#name').val();
-        var class_ = $('#class').val();
-        var batch_no = $('#batch_no').val();
-        var status = $('#status').val();
-        var div_name = $('#div_name').val();
-        var status = $('#status').val();
+        var course = $('#course').val();
+        var e_contact = $('#e_contact').val();
+        var father_name = $('#father_name').val();
+        var religion = $('#religion').val();
 
-        if (oc_no == '') {
+        if (officer_name == '') {
             validate = 1;
-            $('#oc_no').addClass('red-border');
+            $('#officer_name').addClass('red-border');
         }
         if (pno == '') {
             validate = 1;
             $('#pno').addClass('red-border');
         }
-        if (name == '') {
+        if (course == '') {
             validate = 1;
-            $('#name').addClass('red-border');
+            $('#course').addClass('red-border');
         }
-        if (class_ == '') {
+        if (e_contact == '') {
             validate = 1;
-            $('#class').addClass('red-border');
+            $('#e_contact').addClass('red-border');
         }
-
-        if (batch_no == '') {
+        if (father_name == '') {
             validate = 1;
-            $('#batch_no').addClass('red-border');
+            $('#father_name').addClass('red-border');
         }
-        if (status == '') {
+        if (religion == '') {
             validate = 1;
-            $('#status').addClass('red-border');
+            $('#religion').addClass('red-border');
         }
-        if (div_name == '') {
-            validate = 1;
-            $('#div_name').addClass('red-border');
-        }
-
+        
         if (validate == 0) {
             $('#add_form')[0].submit();
             $('#show_error_new').hide();
